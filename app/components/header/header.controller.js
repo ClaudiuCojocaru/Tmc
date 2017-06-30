8462720f292cc5b475389162706ef61aeade74fa
Maties Nicolae <nicolae.maties@vizolution.co.uk>
2017-06-30 09:29:50 +0300

    30.06.17

---
+++ app/components/header/header.controller.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict';
+    var tmc = angular.module('tmc', []);
+
+    tmc.controller('headerController', headerController);
+
+    tmc.$inject = [
+        '$scope'
+    ];
+
+    function headerController() {
+
+    }
+}());
---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '/app'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use('/node_modules',  express.static(__dirname + '/node_modules'));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+   console.log("App is running on port " + port);
+});
